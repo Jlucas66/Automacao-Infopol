23f06026a00c2e2bd565174997d0e587902964b8
Jlucas66 <Jlucas66@github.com>
2026-03-30 09:19:20 -0300

    algumas mudanças: 1 - site correto do infopol 2 - comentário na parte de cadastro.

--- automation.py
+++ automation.py
@@ -13,25 +13,25 @@ def configurar_driver():
     return driver
 
 def fazer_login(driver, wait, usuario, senha, url):
-    print(f"🌍 Acessando: {url}")
+    print(f" Acessando: {url}")
     driver.get(url)
     
     try:
         # Tenta preencher automaticamente (usando os campos 'usr' e 'pwd' que mapeamos)
         wait.until(EC.presence_of_element_located((By.NAME, "usr"))).send_keys(usuario)
         driver.find_element(By.NAME, "pwd").send_keys(senha)
-        print("⌨️ Dados de login preenchidos...")
+        print("⌨Dados de login preenchidos...")
         
         # O robô vai esperar você clicar em entrar ou resolver um captcha, se houver
         input("Pausa de Segurança: Faça o login manualmente no navegador e depois aperte ENTER aqui no terminal para continuar...")
         
     except Exception as e:
-        print("⚠️ Não encontrei os campos de login. Você já está logado ou a URL mudou?")
+        print(" Não encontrei os campos de login. Você já está logado ou a URL mudou?")
         input("Ajuste a página manualmente e aperte ENTER aqui para prosseguir com o cadastro...")
 
 def realizar_cadastro(driver, wait, dados):
     id_db, nome, cpf, data_nasc = dados
-    print(f"📑 Iniciando preenchimento de: {nome}")
+    print(f" Iniciando preenchimento de: {nome}")
     
     # Aqui entrarão os IDs da página INTERNA do Infopol que você verá após o login
     # Exemplo genérico:
