e8a64e0efcc0d96299e78ad514ee32193d335f9b
Jlucas66 <Jlucas66@github.com>
2026-05-18 10:31:38 -0300

    adaptação para integração com o front

--- app/api/app.py
+++ app/api/app.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.db.database import (
     buscar_pessoa_por_cpf,
@@ -8,6 +9,14 @@ from app.db.database import (
 from app.automation.automation import executar_automacao
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 # =========================
